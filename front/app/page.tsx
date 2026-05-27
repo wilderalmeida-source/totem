ebc0a37254ba5436f2f846772c74660932c71ca0
Wilder <wilder.almeida@sistemacri.com.br>
2026-05-27 07:53:15 -0300

    backend modalidade ajustado

--- front/app/page.tsx
+++ front/app/page.tsx
@@ -2,17 +2,27 @@ import React from 'react'
 import Base from '../components/base'
 import Servicos from '../components/servicos'
 import Historic from '../components/historic'
-export default async function Home() {
-      const services = [{ servico: "Agendamento", ID: 'D' }, { servico: "Atendimento", ID: 'B' }, { servico: "Entrega de Exames", ID: 'C' }]
-      const empresa = process.env.EMPRESA_NOME
-      return (<>
-            <Base key={''} type={'home'} props={<div className="flex w-full h-full flex-col">
-                  <div><h2 className="text-6xl text-center">Olá, seja bem vindo a {empresa}!</h2>
-                        <h6 className="text-4xl text-center mt-4">Escolha uma opção abaixo.</h6>
-                  </div>
-                  {services && <Servicos servicos={services} />}
-                  {<div className="flex flex-col h-full justify-end mb-5 xl:h-[700px]"><Historic /></div>}
-            </div>} />
 
-      </>)
+export const dynamic = 'force-dynamic'; 
+
+export default async function Home() {
+  const services = [{ servico: "Agendamento", ID: 'D' }, { servico: "Atendimento", ID: 'B' }, { servico: "Entrega de Exames", ID: 'C' }]
+  
+  // Como a Home é servidora, ela lê do Docker e injeta direto no texto do HTML
+  const empresa = process.env.EMPRESA_NOME || "Nossa Empresa"
+
+  return (
+    <Base type={'home'} props={
+      <div className="flex w-full h-full flex-col">
+        <div>
+          <h2 className="text-6xl text-center">Olá, seja bem vindo ao {empresa}!</h2>
+          <h6 className="text-4xl text-center mt-4">Escolha uma opção abaixo.</h6>
+        </div>
+        {services && <Servicos servicos={services} />}
+        <div className="flex flex-col h-full justify-end mb-5 xl:h-[700px]">
+          <Historic />
+        </div>
+      </div>
+    } />
+  )
 }
--- front/components/base.tsx
+++ front/components/base.tsx
@@ -1,14 +1,26 @@
-import React,{ ReactElement } from "react"
+import React, { ReactElement } from "react"
 
-export default function Base(props:ReactElement){
-    
-    return(
+// Força o componente Base a ler o Docker Compose em tempo de execução
+export const dynamic = 'force-dynamic';
+
+interface BaseProps {
+  props: ReactElement;
+  type?: string;
+  key?: string;
+}
+
+export default function Base({ props }: BaseProps) {
+
+  return (
     <>
-      <div className=" bg-background w-full h-screen flex flex-col">
+      <div className="bg-background w-full h-screen flex flex-col">
         <div className="w-1/4 max-w-48">
-        </div >
-        {props.key}{props.props}
-    </div>
+          {/* Espaço da logo */}
+        </div>
+        
+        {/* Renderiza a página do cliente normalmente */}
+        {props}
+      </div>
     </>
-    )
+  )
 }
